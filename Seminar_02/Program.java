fa2ea769fe6d11cd41cfba42a8aecea998218bf6
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-01-18 19:48:38 +0300

    Создан интерфейс

---
+++ Seminar_02/Program.java
@@ -0,0 +1,7 @@
+package Seminar_02;
+
+public interface Program {
+    int getNext(); // Возвратить следующее по порядку число
+    void reset(); // Делает сброс
+    void setStart(int x); // Установить начальное значение    
+}
