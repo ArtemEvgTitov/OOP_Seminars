c32271fbb2089f29fd04f1b0dab1874f58db506e
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-01-18 20:05:31 +0300

    Создан Test2

---
+++ Seminar_02/Test2.java
@@ -0,0 +1,25 @@
+package Seminar_02;
+
+public class Test2 implements Program {
+    int start;
+    int value;
+
+    @Override
+    public int getNext() {
+        value += 3;
+
+        return value;
+    }
+
+    @Override
+    public void reset() {
+        start = 0;
+        value = 0;
+    }
+
+    @Override
+    public void setStart(int x) {
+        start = x;
+        value = x;
+    }
+}
