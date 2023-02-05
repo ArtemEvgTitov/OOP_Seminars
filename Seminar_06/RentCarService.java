198a8f02886f4e5a2b1dcd7f8c4191015143e32b
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-02-05 18:51:46 +0300

    Создана папка для 6 семинара

---
+++ Seminar_06/RentCarService.java
@@ -0,0 +1,30 @@
+package Seminar_06;
+
+public class RentCarService {
+    public Car findCar(String carNumber){
+        // find car by number
+        return car;
+    }
+
+    public orderCar (String carNumber, Client client){
+        // Client order car
+        return order;
+    }
+
+    public void printOrder(Order order){
+        // print order
+    }
+
+    public void sendMessage(String typeMessage, String message){
+        if(typeMessage.equals("email")){
+            // email 
+            // use java mail sender API
+        }
+    }
+
+    public void getCarInfo(String carType){
+        if(carType.equals("sedan")){
+            // INFO car
+        }
+    }
+}
