c4248d86891709c0703b60f18b55b1a6fb18e881
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-02-08 09:25:54 +0300

    Создан Pirrot

--- Seminar_07/OOP.py
+++ Seminar_07/OOP.py
@@ -1,1 +1,13 @@
-class 
+class Horse():
+    is_horse = True
+
+class Donkey(): # Наследование указывается в скобках
+    is_donkey = True
+
+class Mulle(Horse, Donkey): # Множественное наследование есть только в Python
+    pass
+
+mull = Mulle()
+
+print(mull.is_donkey)
+print(mull.is_horse)
---
+++ Seminar_07/Pirot.py
@@ -0,0 +1,14 @@
+class Pirrot():
+    # Атрибут класса
+    spec = "Птица"
+
+    # Конструктор
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Создаём экземпляр класса
+kesha = Pirrot("Кеша", 10)
+zhorik = Pirrot("Жора", 15)
+
+print()
